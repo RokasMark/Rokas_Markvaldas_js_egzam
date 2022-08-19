69fc658d7dbffa2a3d81b4babeeeb571991199c5
RokasMark <marrkis@gmail.com>
2022-08-19 17:04:05 +0300

    task_7_ok

--- Task 7/script.js
+++ Task 7/script.js
@@ -10,3 +10,11 @@ const audi = {
   year: 2005,
   color: 'white',
 };
+
+
+function showObjectKeys(obj) {
+  return Object.keys(obj);
+}
+
+console.log('"key" masyve  ==>', showObjectKeys(audi));
+
