a2d0884eb0696e0fb4f9dac12eada966986b969b
RokasMark <marrkis@gmail.com>
2022-08-19 16:01:46 +0300

    kazkas padaryta

--- Task 1/script.js
+++ Task 1/script.js
@@ -8,3 +8,6 @@ pamatyti jo pateikto svorio kovertavimą į:
 Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
+
+
+bandom biski gitaaaaaaa
