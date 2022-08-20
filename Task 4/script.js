8c75b98525a7c0912a4d143104d9894abc98c61e
RokasMark <marrkis@gmail.com>
2022-08-20 14:20:39 +0300

    task_4_OK_no_css

--- Task 4/script.js
+++ Task 4/script.js
@@ -8,5 +8,25 @@ nurodomas gamintojas ir jo pagaminti modeliai.
 Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), turi 
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
+'use strict';
+console.log('script.js');
 
 const ENDPOINT = 'cars.json';
+const outputEl = document.getElementById('output');
+
+async function getBrandsData() {
+    const resp = await fetch(ENDPOINT);
+    const brandData = await resp.json();
+    return brandData;
+}
+
+async function showBrands() {
+   let cars = await getBrandsData();
+    cars.forEach(element => {
+        const CardDiv = document.createElement('div');
+        CardDiv.innerHTML = `<ul> ${element.brand} </ul>  <li> ${element.models} </li> ` ;
+        outputEl.append(CardDiv)
+    }); 
+}
+
+showBrands()
