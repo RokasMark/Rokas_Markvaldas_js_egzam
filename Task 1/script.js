8f4410a874e66be7dff16d1e2701093d7f351511
RokasMark <marrkis@gmail.com>
2022-08-20 12:12:03 +0300

    task_1_OK_no_css

--- Task 1/script.js
+++ Task 1/script.js
@@ -9,4 +9,22 @@ Pastaba: atvaizdavimas turi būti matomas pateikus formą ir pateikiamas
 <div id="output"></div> viduje, bei turi turėti bent minimalų stilių;
 ------------------------------------------------------------------- */
 
+'use strict';
+console.log('script.js');
 
+const formEl = document.forms[0];
+const btnEl = document.getElementById('submit-btn')
+const outputEl = document.getElementById('output')
+const inputEl = document.getElementById('search')
+
+
+formEl.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+        const lb = inputEl.value * 2.2046;
+        const g = inputEl.value / 0.001;
+        const oz = inputEl.value * 35.274;
+        outputEl.innerHTML = `weight in kilograms ${inputEl.value} converted to pounds ${lb} to grams ${g} to ozt ${oz}`
+        inputEl.value = '';
+    });
+    
